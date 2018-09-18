d1ea44387ef33fdc4f9d3085f90fc6123e7ea7af
Talisa Erazo <talisaerazo@gmail.com>
2018-09-18 04:01:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function deleteFromObjectByKey(object, key){
   return newObj;
 }
 function destructivelyDeleteFromObjectByKey(object, key){
-  delete object.key;
+  delete object[key];
   return object;
 }
