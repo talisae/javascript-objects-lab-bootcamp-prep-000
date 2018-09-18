ac19453c33310da29cbfefac394e520e85059be0
Talisa Erazo <talisaerazo@gmail.com>
2018-09-18 03:54:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, object, { [key]: value })
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  object[key] = value
-  return object
+  object[key] = value;
+  return object;
 }
